7a8949b3c263b4b2148596b698ddb196cc000fd3
amgolubenny <amgolubenny@gmail.com>
2020-01-23 17:23:50 +0200

    8

---
+++ src/task8.java
@@ -0,0 +1,7 @@
+public class task8 {
+    public static void main(String[] args) {
+        int a = 2, b = 3;
+        int x = (a + 4 * b) * (a - 3 * b) + a^2;
+        System.out.println(x);
+    }
+}
