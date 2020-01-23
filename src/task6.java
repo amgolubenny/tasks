c5ed71e7551c39a17f4d0198eef7c1d16b46aabe
amgolubenny <amgolubenny@gmail.com>
2020-01-23 17:15:05 +0200

    5

---
+++ src/task6.java
@@ -0,0 +1,7 @@
+//Вывести на экран результат вычисления 1+2−4
+public class task6 {
+    public static void main(String[] args) {
+        int i = 1 + 2 - 4;
+        System.out.println(i);
+    }
+}
