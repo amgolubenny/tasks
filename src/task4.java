e4f6be9e052f61eba29cc91cbf19ff1588911c40
amgolubenny <amgolubenny@gmail.com>
2020-01-23 16:06:37 +0200

    1-3

---
+++ src/task4.java
@@ -0,0 +1,12 @@
+public class task4 {
+    public static void main(String[] args) {
+        String s = "A";
+        for (int i = 0; i < 5; i++){
+            System.out.print(s);
+            for (int n = 0; n < 8; n++){
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+    }
+}
