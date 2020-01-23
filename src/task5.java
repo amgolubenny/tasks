8c431d85abad2f9d12557cbfe82c44cbf4d3c36f
amgolubenny <amgolubenny@gmail.com>
2020-01-23 16:45:24 +0200

    5

--- src/task5.java
+++ src/task5.java
@@ -1,10 +1,10 @@
 //Вывести на экран букву "W" из символов "*" (звездочка)
     public class task5 {
     public static void main(String[] args) {
-       System.out.println("*      *       *");
-       System.out.println(" *    * *     *");
-       System.out.println("  *  *   *   *");
-       System.out.println("   *         *");
+       System.out.println("*     *     *");
+       System.out.println(" *   * *   *");
+       System.out.println("  * *   * *");
+       System.out.println("   *     *");
 
     }
 }
