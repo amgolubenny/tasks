087f37f141375839e26864aa96a612e1a452e1fb
amgolubenny <amgolubenny@gmail.com>
2020-01-30 14:30:09 +0200

    11

--- src/Arithmetic/task11.java
+++ src/Arithmetic/task11.java
@@ -1,5 +1,6 @@
 package Arithmetic;
 //Вычислите значение выражения ((|x−5|−sinx)/3) + √(x^2+2014) * cos2*x − 3 при x=−2.34
+// Задача 11 на сайте http://www.itmathrepetitor.ru/prog/zadachi-na-vychisleniya/
 public class task11 {
     public static void main(String[] args) {
         double x = -2.34;
